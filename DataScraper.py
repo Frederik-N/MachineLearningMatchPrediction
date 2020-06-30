a675e80dc8ab05cbc7a363c1688e0b0ed02b8c22
Dev.Nielsen <Frederik-N@hotmail.dk>
2020-06-30 15:24:33 +0200

    mainclassifier added example test

--- DataScraper.py
+++ DataScraper.py
@@ -1,12 +1,51 @@
 # Scrape data of CSGO matches
+import math
+import re as re
+import requests
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup as bs
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import learning_curve
 
 class Scrape():
     def __init__(self):
         super().__init__()
 
     # function to scrape data from HLTV.com
-    def scrapeFromHLTV():
+    def scrapeFromHLTV(self):
+
+        # Top team URLS (for scraping match data)
+        team = ['https://www.hltv.org/stats/teams/matches/6665/astralis']
+        # Pull/scrape/parse data
+        r = requests.get(team)
+        root = bs(r.content, "html.parser")
+        root.prettify()
+        table = (str)(root.find("table"))
+        data = pd.read_html(table, header=0)[0]
+
+        # Give columns proper names
+        data.columns = ['Date', 'Event', 'Opponent', 'Map', 'Rating', 'W/L', 'Outcome']
+
+        # Clean win/loss data and turn to numeric format for compatibilty with machine learning classifier
+        data.loc[data.Outcome == 'W', 'Outcome'] = 1
+        data.loc[data.Outcome == 'L', 'Outcome'] = 0
+        data.loc[data.Outcome == 'T', 'Outcome'] = 0
+
+        # Misc sanitation (datetime conversion, typecasting)
+        #data['Outcome'] = data['Outcome'].astype(str).astype(int)
+        #data['Event'] = data['Event'].astype(str)
+        #data['Date'] = pd.to_datetime(data['Date'])
+        #data['Year'] = data['Date'].dt.year
+        #data['Team'] = topTeams[i]
+
+        # Print resultant dataframe of all merged together top 10 team historic match results
+        return data
 
 
 if __name__ == '__main__':
-    print("")
+    scrape = Scrape()
+    print(scrape.scrapeFromHLTV())
+
--- MainClassifier.py
+++ MainClassifier.py
@@ -12,20 +12,56 @@ from tensorflow import keras
 from pandas import read_csv
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-class MainClassifier():
-    
-    def __init__(self):
-        self.params = None
-
-    #Firstly import data from the training and testing folders (TODO scrape training and testing data of csgo matches (make new class))
-    def importData():
-        pass
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 
 if __name__ == "__main__":
-    url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
-    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+
+    # import data
+    url = "https://raw.githubusercontent.com/Frederik-N/MachineLearningMatchPrediction/master/data/data.csv"
+    names = ['team1', 'team2', 'class']
     dataset = read_csv(url, names=names)
-    print(dataset.shape)
-    print(dataset.head(3))
+
+    # Split-out validation dataset from training set
+    array = dataset.values
+    X = array[:,0:2]
+    y = array[:,2]
+    X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1, shuffle=True)
+
+    # training and testing sets
+    print(X_train,Y_train, "training data")
+    print(X_validation, Y_validation, "testing data")
+
+    # initialize different models to test which one works the best
+    models = []
+    models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+    models.append(('LDA', LinearDiscriminantAnalysis()))
+    models.append(('KNN', KNeighborsClassifier()))
+    models.append(('CART', DecisionTreeClassifier()))
+    models.append(('NB', GaussianNB()))
+    models.append(('SVM', SVC(gamma='auto')))
+
+    # evaluate each model in turn
+    results = []
+    names = []
+    for name, model in models:
+        kfold = StratifiedKFold(n_splits=2, random_state=1, shuffle=True)
+        cv_results = cross_val_score(model, X_train, Y_train, cv = kfold, scoring='accuracy')
+        results.append(cv_results)
+        names.append(name)
+        print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+        
+    # Compare Algorithms
+    plt.boxplot(results, labels=names)
+    plt.title('Algorithm Comparison')
+    plt.show()
+
+
+    
